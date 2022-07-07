d724cd0df392e5ac304f495a073612e2857ecd78
anirudh-pec <104815198+anirudh-pec@users.noreply.github.com>
2022-07-07 23:49:10 +0530

    Add files via upload

---
+++ PEC Assignment 6 output.py
@@ -0,0 +1,62 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:\Users\91700\Documents\PEC Assignment 6.py ============
+Question 1
+Enter number to check if perfect or not : 28
+Yes, it is a perfect number.
+----------------------------------------------------
+Question 2
+Enter input string to check for palindrome : nurses run
+Yes, it is a palindrome.
+----------------------------------------------------
+Question 3
+Enter number of rows of Pascal Triangle : 7
+        1 
+       1 1 
+      1 2 1 
+     1 3 3 1 
+    1 4 6 4 1 
+   1 5 10 10 5 1 
+  1 6 15 20 15 6 1 
+----------------------------------------------------
+Question 4
+Enter string to be checked if pangram or not : a quick brown fox jumps over the lazy dog
+Yes, it is a pangram.
+----------------------------------------------------
+Question 5
+Enter hyphen separated sequence of words : banana-orange-apple-tomato-pineapple
+Alphabetically sorted hyphen separated list is : 
+apple-banana-orange-pineapple-tomato
+----------------------------------------------------
+Question 6
+Student ID :  21107091
+Student name :  Anirudh Ralhan
+
+Student ID :  21107091
+Student class :  Mechanical
+
+Student ID :  21107091
+Student name :  Anirudh Ralhan
+Student class :  Mechanical
+----------------------------------------------------
+Question 7
+Checking for instances : 
+True
+False
+True
+False
+Checking whether the said classes are subclasses of the built-in object class or not.
+True
+True
+----------------------------------------------------
+Question 8
+[[-10, 2, 8], [-7, -3, 10]]
+----------------------------------------------------
+Question 9
+True
+True
+False
+False
+False
+----------------------------------------------------
